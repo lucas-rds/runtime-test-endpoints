a23edce9e7ac61852fd0859f387fe62862290d59
Lucas Rodrigues <lucas.rodrigues6673@gmail.com>
2020-11-11 22:31:03 -0300

    endpoints dinamicos

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
-var express = require("express");
-var app = express();
-var fs = require('fs');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const Mustache = require("mustache");
+const multer = require("multer");
 
-app.get("/", (req, res, next) => {
-    const json = fs.readFileSync("./json.json", "utf-8");
-    res.send(json);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + ".json";
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (_, file, next) => {
+    if (
+      file.mimetype === "application/json" &&
+      file.originalname.includes(".json")
+    ) {
+      return next(null, true);
+    }
+    next(null, false);
+  },
+});
+
+const routes = [{ url: "/", json: "" }]; // [{ url:"/path", json: "name.json"}]
+app.get("/", (req, res) => {
+  res.setHeader("Content-type", "text/html");
+  const template = fs.readFileSync("./index.html", "utf-8");
+  res.send(
+    Mustache.render(template, {
+      routes,
+    })
+  );
+});
+
+app.post("/add", upload.single("json"), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  if (req.file && req.body && req.body.path) {
+    routes.push({
+      url: req.body.path,
+      json: req.file.originalname,
+    });
+
+    app.get(`/${req.body.path}`, (_, response) => {
+      const path = "" + req.file.path;
+      const json = fs.readFileSync(`./${path}`, "utf-8");
+      response.send(json);
+    });
+
+    return res.send("added");
+  }
+
+  res.status(400);
+  res.send("Erro");
 });
 
 app.listen(process.env.PORT || 3000, () => {
- console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
